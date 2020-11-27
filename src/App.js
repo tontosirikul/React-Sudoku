db795dedf6eb35821806d6935baf2f659a8a8215
Ton Tosirikul <tontosirikul@Tons-MacBook-Pro.local>
2020-11-28 00:20:18 +0700

    add ionic ios build

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-template-curly-in-string */
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import React, { Component } from "react";
 import "./App.css";
 import Board from "./Board.js";
-
 //component นี้ return หน้า webpage
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Board></Board>
-      </div>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>SUDOKU</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <div className="App">
+          <Board></Board>
+        </div>
+      </IonPage>
     );
   }
 }
